66f6a282d40a82af55ec2209c6043fb497b0c803
Ashna Shrestha <101237966+chheyl@users.noreply.github.com>
2026-05-02 14:35:33 +0545

    Clean up unused imports in App.jsx
    Removed unused imports from App.jsx to clean up code.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Login from "./component/login.jsx";
 import Dashboard from "./component/dashboard.jsx";
-import AddTransaction from "./component/addtransaction.jsx";
-import TransactionList from "./component/transactionlist.jsx";
-import Charts from "./component/charts";
 
 export default function App() {
   const [token, setToken] = useState(localStorage.getItem('token'))
